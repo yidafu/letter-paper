db93b487f2871c314a5ccf6dfe7cf86e8e47c452
yidafu <tiaozuwushi@qq.com>
2019-01-22 23:51:01 +0800

    feat(squid/Button/): the first edition of button component
    custom styled button, simple animation.
    
    re #2 re #5

---
+++ src/squid/Button/Button.vue
@@ -0,0 +1,67 @@
+<template>
+  <button class="squid-btn .squid-btn-primary">
+    <slot />
+  </button>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/variables.scss';
+
+/**
+ * @see https://css-tricks.com/overriding-default-button-styles/
+ */
+
+@keyframes btn-active {
+  50% {
+    box-shadow: 0 0.5em 5px 0px #aaa;
+  }
+  100% {
+    box-shadow: 0px 1px 5px 0px #aaa;
+  }
+}
+.squid-btn:not(:disabled) {
+  // @see https://developer.mozilla.org/en-US/docs/Web/CSS/cursor
+  // image size limit to 32x32
+  cursor: url(../asset/pointer-outline.png) , pointer;
+}
+.squid-btn {
+  display: inline-block;
+  font-weight: 500;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  user-select: none;
+  padding: 0.5em 1em;
+  font-size: 1rem;
+  border: 1px solid transparent;
+  border-radius: .25em;
+  box-shadow: 0px 2px 5px 0px #aaa;
+}
+
+.squid-btn:hover {
+  opacity: 0.7;
+}
+
+.squid-btn:focus,
+.squid-btn:active {
+  outline: none;
+}
+.squid-btn:active {
+  animation: btn-active;
+  animation-duration: 0.3s;
+  animation-iteration-count: 1;
+  animation-timing-function: ease;
+}
+
+.squid-btn-primary {
+  background-color: $primary-color;
+  color: #fff;
+}
+
+</style>
---
+++ src/squid/Button/Wave.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>
+    <span class="wave"></span>
+    <slot />
+  </span>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
