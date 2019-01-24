890f62daa977e4691ad0d6ecf469996deb56fb17
yidafu <tiaozuwushi@qq.com>
2019-01-24 17:09:05 +0800

    style(mock/): add sime
    re #7

--- mock/api.js
+++ mock/api.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const Mock = require('mockjs')
+const Mock = require('mockjs');
 
-router.get('/',function( req, res, next ) {
+router.get('/', function(req, res, next) {
   res.json(
     Mock.mock({
       'count|21-29': 21,
@@ -10,12 +10,12 @@ router.get('/',function( req, res, next ) {
         'title': Mock.mock('@ctitle'),
         'summary': Mock.mock('@cparagraph(5, 7)'),
         'date': Mock.mock('@date("yyyy-MM-dd")'),
-        'tag|3-6': [ Mock.mock('@cword(3)') ],
+        'tag|3-6': [Mock.mock('@cword(3)')],
         'category': Mock.mock('@cword(3)'),
       }]
     })
-  )
-})
+  );
+});
 
 router.get('/post/:id', function(req, res, next) {
   res.json(
@@ -24,25 +24,25 @@ router.get('/post/:id', function(req, res, next) {
       'title': Mock.mock('@ctitle'),
       'content': Mock.mock('@cparagraph(50, 70)'),
       'date': Mock.mock('@date("yyyy-MM-dd")'),
-      'tag|3-6': [ Mock.mock('@cword(3)') ],
+      'tag|3-6': [Mock.mock('@cword(3)')],
       'category': Mock.mock('@cword(3)'),
       'next': Mock.mock('@id'),
       'pre': Mock.mock('@id'),
     })
-  )
-})
+  );
+});
 
 router.get('/tags', function (req, res, next) {
   res.json(
-    Array( Math.round(Math.random() * 10) + 1 ).fill(Mock.mock({
+    Array(Math.round(Math.random() * 10) + 1).fill(Mock.mock({
       'name': Mock.mock('@cword(2,4)'),
-      'tags|3-9': [ {
+      'tags|3-9': [{
         'id': Mock.mock('@id'),
         'title': Mock.mock('@ctitle'),
-      } ],
+      }],
     }))
-  )
-})
+  );
+});
 
 router.get('/tags/:tags', function (req, res, next) {
   res.json(
@@ -53,8 +53,8 @@ router.get('/tags/:tags', function (req, res, next) {
         'title': Mock.mock('@ctitle'),
       }],
     })
-  )
-})
+  );
+});
 
 router.get('/archive', function (req, res, next) {
   res.json(
@@ -65,8 +65,8 @@ router.get('/archive', function (req, res, next) {
         'title': Mock.mock('@ctitle'),
       }],
     }))
-  )
-})
+  );
+});
 
 router.get('/archive/:date', function (req, res, next) {
   res.json(
@@ -77,7 +77,7 @@ router.get('/archive/:date', function (req, res, next) {
         'title': Mock.mock('@ctitle'),
       }],
     })
-  )
-})
+  );
+});
 
 module.exports = router;
