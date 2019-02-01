795288811fe018588529bcbd5702532c06eab893
yidafu <tiaozuwushi@qq.com>
2019-02-01 14:15:02 +0800

    refactor(server/): pipe data to json files

--- server/config.js
+++ server/config.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
 module.exports = {
   repo: {
     name: 'posts',
     path: 'https://github.com/yidafu/blog-post.git',
-  }
+  },
+  output: {
+    dir: path.resolve(__dirname, '../mock'),
+  },
 };
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const clone = require('git-clone');
 const path = require('path');
 const YAML = require('yaml');
 
-const { repo } = require('./config');
+const { repo, output } = require('./config');
 
 if (!fs.existsSync(path.join(__dirname, repo.name))) {
   clone(repo.path, repo.name, function() {
@@ -19,19 +19,41 @@ const MdFiles = glob.sync(
     ignore: '**/README.md',
   },
 );
+const DBDir = output.dir // path.join(__dirname, output.dir);
 
-const posts = MdFiles.map(function(filePath) {
-  const file = fs.readFileSync(filePath, 'utf8').trim();
-  if (file.startsWith('---')) {
-    const dashIdx = file.indexOf('---', 3);
-    const frontmatter = YAML.parse(file.substring(3, dashIdx));
-    // + 3 to trim `---`
-    const content = file.substring(dashIdx + 3);
-    return { ...frontmatter, content, summay: content.substr(0, 200) };
-  } else {
-    throw new Error(filePath + ' must has a frontmatter and starts with `---`');
-  }
-});
+if (!fs.existsSync(DBDir)) {
+  fs.mkdirSync(DBDir, { recursive: true})
+}
 
-console.log(posts);
+const posts = MdFiles
+  .map(function(filePath) {
+    const file = fs.readFileSync(filePath, 'utf8').trim();
+    if (file.startsWith('---')) {
+      const dashIdx = file.indexOf('---', 3);
+      const frontmatter = YAML.parse(file.substring(3, dashIdx));
+      // + 3 to trim `---`
+      const content = file.substring(dashIdx + 3);
+      const {name} = path.parse(filePath)
+      return { name, ...frontmatter, content, summary: content.substr(0, 200) };
+    } else {
+      throw new Error(filePath + ' must has a frontmatter and starts with `---`');
+    }
+  })
+  .sort( (pre, next) => +new Date(pre.date) > +new Date(next.date) )
+  .map((post, idx) => {
+    post.id = idx + 1; // id should start with 1
+    fs.writeFileSync(
+      path.join(DBDir, post.id + '.json'), 
+      JSON.stringify(post, null, 2),
+    );
+    // post.pre = post.id - 1 > 1 ? post.id : null;
+    // post.next = post.id - 1 > 1 ? post.id : null;
+    const {content, ...summaryPost} = post;
+    return summaryPost;
+  })
 
+// db/index.json
+fs.writeFileSync(
+  path.join(DBDir, 'index.json'),
+  JSON.stringify( {count: posts.length, posts}, null, 2)
+)
